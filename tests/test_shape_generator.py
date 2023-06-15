a1dcb07704b3cfd43943275d427ee46dabaab37c
Kev Lopez <kevinlopezandrade@gmail.com>
2023-06-15 17:53:39 +0200

    Add unit tests for ShapeGenerator.

---
+++ tests/test_shape_generator.py
@@ -0,0 +1,28 @@
+import random
+
+from arcworld.dsl.functional import normalize
+from arcworld.shape.dsl.generator import ShapeGenerator
+
+
+def test_no_augmentations_no_variations():
+    max_pixels = random.randint(10, 100)
+
+    generator = ShapeGenerator(
+        max_pixels=max_pixels, max_variations=1, augmentations=[]
+    )
+
+    shapes = generator.generate_random_proto_shapes()
+
+    assert len(shapes) == max_pixels
+
+    sizes = set(i for i in range(1, max_pixels + 1))
+    current_sizes = set(len(shape) for shape in shapes)
+
+    assert sizes == current_sizes
+
+
+def test_normalized_shapes():
+    generator = ShapeGenerator()
+    shapes = generator.generate_random_proto_shapes()
+
+    assert all(normalize(shape) == shape for shape in shapes)
