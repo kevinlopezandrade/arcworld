e97218ca5da44c00a3f09c7bc6ee6712d120be49
Kev Lopez <kevinlopezandrade@gmail.com>
2023-10-02 12:29:42 +0200

    Add examples of generating data.

---
+++ examples/gravity/drop.py
@@ -0,0 +1,88 @@
+import random
+from typing import Optional
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from tqdm import tqdm
+
+from arcworld.dsl.arc_types import Shapes
+from arcworld.internal.constants import Example, Task
+from arcworld.schematas.oop.drop.transforms import (
+    DropBidirectional,
+    DropBidirectionalDots,
+    Gravitate,
+)
+from arcworld.shape.dsl.generator import ShapeGeneratorDSL
+from arcworld.storage.fingerprint import hash_task, normalize_task
+from arcworld.storage.table import Base, Schemata
+
+AUTHOR = "kevinlo@student.ethz.ch"
+
+
+class Gravity(Schemata):
+    __tablename__ = "gravity"
+
+
+def generate(N_tasks: int, N_examples: int, seed: Optional[int] = None):  # noqa
+    if seed:
+        print(f"Seed: {seed}")
+
+    random.seed(seed)
+
+    generator = ShapeGeneratorDSL(max_variations=30)
+    shapes: Shapes = generator.generate_random_shapes()
+    print("Total number of generated shapes: ", len(shapes))
+
+    for _ in tqdm(range(N_tasks), desc="Generating tasks"):
+        transform = random.choice(
+            [Gravitate, DropBidirectional, DropBidirectionalDots]
+        )()
+
+        filtered_shapes = shapes
+        for filter in transform.filters:
+            filtered_shapes = filter.filter(filtered_shapes)
+
+        sampler = transform.grid_sampler()
+
+        task: Task = []
+        try:
+            for _ in range(N_examples):
+                grid_builder = sampler()
+                input_grid = grid_builder.build_input_grid(filtered_shapes)
+                ouput_grid = transform.transform(input_grid)
+
+                example = Example(input=input_grid.grid_np, output=ouput_grid.grid_np)
+
+                task.append(example)
+        except Exception:
+            pass
+        else:
+            yield task, transform.program
+
+
+engine = create_engine("sqlite:////Users/kev/tasks.db", echo=False)
+
+N = 20000
+succesful = 0
+# seed = random.randint(1, 1000)
+seed = 13
+store = True
+
+with Session(engine) as session:
+    if store:
+        Base.metadata.create_all(engine)
+
+    for task, program in generate(N, 4, seed):
+        if store:
+            session.add(
+                Gravity(
+                    id=hash_task(task),
+                    author=AUTHOR,
+                    transformation=program,
+                    task=normalize_task(task),
+                )
+            )
+            session.commit()
+        succesful += 1
+
+print(f"Succesfull ratio {succesful}/{N}")
